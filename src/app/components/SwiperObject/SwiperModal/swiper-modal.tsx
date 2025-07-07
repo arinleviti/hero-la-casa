53c18cf9212608878dc1177d6b67878d14ce3752
arinleviti <arin.leviti@gmail.com>
2025-07-07 22:46:33 +0200

    category icons
    placed category icons in the modal

--- src/app/components/SwiperObject/SwiperModal/swiper-modal.tsx
+++ src/app/components/SwiperObject/SwiperModal/swiper-modal.tsx
@@ -9,14 +9,49 @@ interface Props {
   onClose: () => void;
 }
 
+const getCategoryIconPath = (category: string): string => {
+  switch (category) {
+    case 'manzo':
+      return '/CategoryIcons/meat.png';
+    case 'vegano':
+      return '/CategoryIcons/vegan.png';
+    case 'vegetariano':
+      return '/CategoryIcons/vegetarian.png';
+    case 'gluten-free':
+      return '/CategoryIcons/gluten-free.png';
+    case 'pesce':
+      return '/CategoryIcons/fish.png';
+    case 'leggermente piccante':
+      return '/CategoryIcons/mildlyHot.png';
+    case 'piccante':
+      return '/CategoryIcons/hot.png';
+    default:
+      return '/images/default-icon.png';
+  }
+}
+
 const BootstrapBurgerModal: React.FC<Props> = ({ burger, onClose }) => {
   if (!burger) return null;
 
   return (
     <Modal show={true} onHide={onClose} centered dialogClassName={styles.modalDialog}
-  contentClassName={styles.modalContent}>
+      contentClassName={styles.modalContent}>
       <Modal.Header closeButton className={styles.modalHeader}>
-        <Modal.Title  className={styles.modalTitle}>{burger.name}</Modal.Title>
+        <div className={styles.headerContent}>
+        <Modal.Title className={styles.modalTitle}>{burger.name}</Modal.Title>
+        <div className={styles.categoryIcons}>
+          {burger.categories.map((category, i) => (
+            <Image
+              key={i}
+              src={getCategoryIconPath(category)}
+              alt={category}
+              width={35}
+              height={35}
+              title={category}
+            />
+          ))}
+        </div>
+        </div>
       </Modal.Header>
       <Modal.Body className={styles.modalBody}>
         <div className={styles.imageWrapper}>
@@ -36,8 +71,8 @@ const BootstrapBurgerModal: React.FC<Props> = ({ burger, onClose }) => {
         </ul>
       </Modal.Body>
       <Modal.Footer className={styles.modalFooter}>
-        <Button variant="secondary" onClick={onClose}>
-          Close
+        <Button className={styles.reserveButton} onClick={onClose}>
+          RISERVA UN TAVOLO
         </Button>
       </Modal.Footer>
     </Modal>
