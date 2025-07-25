0f3e806abeb1c63df182fc867fe558cd194a0476
arinleviti <arin.leviti@gmail.com>
2025-07-25 15:43:26 +0200

    seeded the database
    with the updated data. now i need to fix the blog page styling

--- src/app/components/LandingPage/overlay.tsx
+++ src/app/components/LandingPage/overlay.tsx
@@ -1,11 +1,18 @@
 'use client'
-
+import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
 export default function Overlay() {
   const [showOverlay, setShowOverlay] = useState(true)
+  const pathname = usePathname();
 
   useEffect(() => {
+
+    if (pathname !== '/') {
+      setShowOverlay(false)
+      return
+    }
+
     function handleScroll() {
       setShowOverlay(false)
       window.removeEventListener('scroll', handleScroll)
@@ -14,7 +21,9 @@ export default function Overlay() {
     window.addEventListener('scroll', handleScroll)
 
     return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
+  }, [pathname])
+  // Don't render anything if not on homepage
+  if (pathname !== '/') return null
 
   return (
     <>
