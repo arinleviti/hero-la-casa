0b525c99112ce50825ec72c882fede1c25d831f2
arinleviti <arin.leviti@gmail.com>
2025-08-20 20:51:14 +0200

    added sitemap
    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://herolacasadelburger.it', // <-- replace with your domain
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
+
+export default config;
